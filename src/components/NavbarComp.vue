59befb42e2a12ab3b0849e5c49b1e74333f7bf17
sihlandlo-schillo <sihlandlom@gmail.com>
2024-06-24 15:50:02 +0200

    committing again

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,9 @@
+.<template>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/consoles">Consoles</router-link> |
+        <router-link to="/cars">Cars</router-link> |
+        <router-link to="/friends">Friends</router-link>
+    </nav>
+</template>
